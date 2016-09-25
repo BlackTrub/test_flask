1fe87cab144bcafc0cc0fa932ade5d71d92a09f5
BlackTrub <lightlocdog@gmail.com>
2016-09-25 22:09:55 +0300

    add more page;

--- test_run.py
+++ test_run.py
@@ -2,10 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
 
+@app.route('/')
+def home_page():
+    return 'Home page'
+
+
+@app.route('/hello')
+def hello_page():
+    return 'Hello page'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post №%d' % post_id
 
 if __name__ == '__main__':
     app.run()
